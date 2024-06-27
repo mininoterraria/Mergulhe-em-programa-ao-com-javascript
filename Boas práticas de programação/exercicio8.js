7e3275535eb895a196888b2a79977197df4b0168
mininoterraria <128259891+mininoterraria@users.noreply.github.com>
2024-06-27 14:13:52 -0300

    Add files via upload

---
+++ Boas práticas de programação/exercicio8.js
@@ -0,0 +1,9 @@
+//Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem
+let numero = prompt("Insira um número");
+if(numero > 0){
+    alert("É positivo.");
+}else if(numero == 0){
+    alert("É zero.");
+}else{
+    alert("É negativo");
+}
---
+++ Boas práticas de programação/exercicio9.js
@@ -0,0 +1,6 @@
+//Use um loop while para imprimir os números de 1 a 10 no console
+contador = 1;
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
